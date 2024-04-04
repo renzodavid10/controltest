281b084869e17520df137fe08e9c65200a65ab56
renzodavid10 <renzo.torres110@gmail.com>
2024-04-04 01:54:26 -0500

    conexion-url

--- index.php
+++ index.php
@@ -3,9 +3,9 @@ session_start();
 
 
 if (isset($_SESSION['S_USUARIO'])) {
-    echo $_SESSION['S_USUARIO'];
-    echo $_SESSION['S_IDUSUARIO'];
-    //header('Location: views/index.php'); /// si mi inicion esta creada me manda a la pagina
+    //echo $_SESSION['S_USUARIO'];
+    //echo $_SESSION['S_IDUSUARIO'];
+    header('Location: views/index.php'); /// si mi inicion esta creada me manda a la pagina
 }
 ?>
 <!DOCTYPE html>
