776ffbb715a0508875536d85e003d0db2e0cec27
renzodavid10 <renzo.torres110@gmail.com>
2024-04-06 02:01:54 -0500

    recarga listado

--- views/reunion/reunion.php
+++ views/reunion/reunion.php
@@ -325,8 +325,32 @@
                             </div>
                         </a>
                         <div id="collapseTwo" class="collapse" data-parent="#accordion">
-                            <div class="card-body">
-                                Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                            <div class="form-group">
+                                <textarea id="compose-textarea" class="form-control" style="height: 300px">
+                      <h1><u>Heading Of Message</u></h1>
+                      <h4>Subheading</h4>
+                      <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain
+                        was born and I will give you a complete account of the system, and expound the actual teachings
+                        of the great explorer of the truth, the master-builder of human happiness. No one rejects,
+                        dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know
+                        how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again
+                        is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain,
+                        but because occasionally circumstances occur in which toil and pain can procure him some great
+                        pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise,
+                        except to obtain some advantage from it? But who has any right to find fault with a man who
+                        chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that
+                        produces no resultant pleasure? On the other hand, we denounce with righteous indignation and
+                        dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so
+                        blinded by desire, that they cannot foresee</p>
+                      <ul>
+                        <li>List item one</li>
+                        <li>List item two</li>
+                        <li>List item three</li>
+                        <li>List item four</li>
+                      </ul>
+                      <p>Thank you,</p>
+                      <p>John Doe</p>
+                    </textarea>
                             </div>
                         </div>
                     </div>
@@ -353,13 +377,13 @@
 </section>
 
 <script>
-    listar_tarea();
+   // listar_tarea();
     listar_usu();
     $(document).ready(function() {
         $('.js-example-basic-single').select2();
         console.log("hola")
     });
-    
+
     contar_tarea('No iniciada');
     listar_empresa()
     //listar_tmiembro();
