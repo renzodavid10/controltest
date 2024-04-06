530ef15d79c3ebec339c7eae90c325c9430661c8
renzodavid10 <renzo.torres110@gmail.com>
2024-04-06 01:33:48 -0500

    recarga register2

--- controller/tarea/crear_tar.php
+++ controller/tarea/crear_tar.php
@@ -1,20 +1,18 @@
 <?php
+session_start();
+require_once('../../model/model_tarea.php');
 
-require_once('../../model/model_tarea.php'); 
-
-$MT= new model_tarea();
+$MT = new model_tarea();
 
 $desc =  htmlspecialchars($_POST['desc'], ENT_QUOTES, 'UTF-8');
-$crit="URGE";
+$crit = "URGE";
 $responsable =  htmlspecialchars($_POST['responsable'], ENT_QUOTES, 'UTF-8');
 $departa =  htmlspecialchars($_POST['departa'], ENT_QUOTES, 'UTF-8');
 $elemento =    htmlspecialchars($_POST['elemento'], ENT_QUOTES, 'UTF-8');
 $fecha =    htmlspecialchars($_POST['fecha'], ENT_QUOTES, 'UTF-8');
+$usu_dni = $_SESSION['S_DNI_PT'];
+$orig = $_SESSION['S_EMPRE'];
 
-session_start();
-$usu_dni=$_SESSION['S_DNI_PT'] ;
-$orig=$_SESSION['S_EMPRE'];
-
-$consultar= $MT->crear_tarea($desc,$crit,$responsable,$departa,$orig,$usu_dni,$fecha);
+$consultar = $MT->crear_tarea($desc, $crit, $responsable, $departa, $orig, $usu_dni, $fecha);
 //echo $orig;
 echo $consultar;
