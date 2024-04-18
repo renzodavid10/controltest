9169188ca365073d7f0c2f39702b58d768b09a10
renzodavid10 <renzo.torres110@gmail.com>
2024-04-17 20:31:16 -0500

    formulario1

--- views/formulario/formulario.php
+++ views/formulario/formulario.php
@@ -10,38 +10,16 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="row font-weight-normal ">
-                    <div class="col-5">
-                        <div class="card">
-                            <div class="card-header">
-                                <h3 class="card-title">Progress bars</h3>
-                            </div>
-                            <!-- /.card-header -->
-                            <div class="card-body">
-                                <div class="progress mb-3">
-                                    <div class="progress-bar bg-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-                                        <span class="sr-only">40% Complete (success)</span>
-                                    </div>
-                                </div>
-                                <div class="progress mb-3">
-                                    <div class="progress-bar bg-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
-                                        <span class="sr-only">20% Complete</span>
-                                    </div>
-                                </div>
-                                <div class="progress mb-3">
-                                    <div class="progress-bar bg-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
-                                        <span class="sr-only">60% Complete (warning)</span>
-                                    </div>
-                                </div>
-                                <div class="progress mb-3">
-                                    <div class="progress-bar bg-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-                                        <span class="sr-only">80% Complete</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- /.card-body -->
+                    <div class="col-4">
+                        <div class="container" style="font-size: medium;">
+                            <p id="user" style="margin-bottom: auto;"><span class="font-weight-bold ">Usuario</span> - Renzo</p>
+                            <p style="margin-bottom: auto;"><span class="font-weight-bold ">Rutina -</span></p>
+                            <p style="margin-bottom: auto;"><span class="font-weight-bold ">Formulario-</span></p>
+                            <p style="margin-bottom: auto;"><span class="font-weight-bold">Enviado - </span></p>
                         </div>
+
                     </div>
-                    <div class="col-2">
+                    <div class="col-3">
                         <p>Porcentaje de proceso</p>
                         <div class="progress progress-sm" style="border-radius: 10px">
                             <div class="progress-bar bg-primary" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
@@ -50,7 +28,7 @@
                         </div>
                         <p>40%</p>
                     </div>
-                    <div class="col-2">
+                    <div class="col-3">
                         <p>Porcentaje de conformidad</p>
                         <div class="progress progress-sm mt-3" style="border-radius: 10px">
                             <div class="progress-bar bg-success" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;border-radius: 10px">
@@ -68,11 +46,8 @@
                         </div>
                         <p>85%</p>
                     </div>
-                    <div class="col-2">
-
-
+                    <div class="col-1">
                     </div>
-
                     <div class="col-1">
                         <button type="button" class="btn btn-primary btn-sm ">
                             <i class="fa-solid fa-download"></i>
@@ -83,42 +58,7 @@
                     </div>
                 </div>
             </div>
-            <div class="card">
-                <div class="card-body">
-                    <div class="row  font-weight-normal">
-                        <div class="col-6">
-                            <label for="">Usuario:</label>
-                            <input type="text" hidden>
-                            <input type="text" class="form-control">
-                        </div>
-                        <div class="col-6">
-                            <div class="card-tools">
-                                <button type="button" class="btn ">
-                                    <i class="fa-solid fa-download"></i>
-                                </button>
-                                <button type="button" class="btn btn-tool" data-card-widget="remove">
-                                    <i class="fas fa-times"></i>
-                                </button>
-                            </div>
-                        </div>
-                        <div class="col-6">
-                            <label for="">Rutina</label>
-                            <input type="text" class="form-control">
 
-                        </div>
-
-                        <div class="col-6">
-                            <label for="">Formulario</label>
-                            <input type="text" class="form-control">
-                        </div>
-                        <div class="col-6">
-                            <label for="">Enviado</label>
-                            <input type="text" class="form-control">
-                        </div>
-
-                    </div>
-                </div>
-            </div>
         </div>
         <!-- FIN DE LA PARTE IZQUIERDA-->
         <div class="col-6">
@@ -154,3 +94,8 @@
 
     </div>
 </section>
+<script>
+$(document).ready(function(){
+    document.getElementById('user').innerHTML+=' jojo';
+});
+</script>
