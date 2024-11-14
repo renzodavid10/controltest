a7334e34eb51bd2ba4865b487bd3ded5b2e61782
RenzoDavid110 <renzo.david110@gmail.com>
2024-11-14 01:54:41 -0500

    Subiendo ultimos cambios

--- js/rutina.js
+++ js/rutina.js
@@ -39,7 +39,7 @@ $("#tb_rd").on('click', 'tr', function () {
                 '</div>' +
                 '</div>' +
                 '<div class="mt-3 col-12" style="display:none" id="div1">' +
-                '<textarea id="descri" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
+                '<textarea id="descri1" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
                 '</div>';
             document.getElementById('cuestionario').innerHTML += '<div class="mt-3 col-12">' +
                 '<label for="pregunta2" id="pr_2" class="form-label"> ' + 2 + ' ' + rowData['cues2'] + '</label>' +
@@ -59,7 +59,7 @@ $("#tb_rd").on('click', 'tr', function () {
                 '</div>' +
                 '</div>' +
                 '<div class="mt-3 col-12" style="display:none" id="div2">' +
-                '<textarea id="descri" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
+                '<textarea id="descri2" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
                 '</div>';
             document.getElementById('cuestionario').innerHTML += '<div class="mt-3 col-12">' +
                 '<label for="pregunta3" id="pr_3" class="form-label"> ' + 3 + ' ' + rowData['cues3'] + '</label>' +
@@ -79,7 +79,7 @@ $("#tb_rd").on('click', 'tr', function () {
                 '</div>' +
                 '</div>' +
                 '<div class="mt-3 col-12" style="display:none" id="div3">' +
-                '<textarea id="descri" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
+                '<textarea id="descri3" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
                 '</div>';
             document.getElementById('cuestionario').innerHTML += '<div class="mt-3 col-12">' +
                 '<label for="pregunta4" id="pr_4" class="form-label"> ' + 4 + ' ' + rowData['cues4'] + '</label>' +
@@ -99,7 +99,7 @@ $("#tb_rd").on('click', 'tr', function () {
                 '</div>' +
                 '</div>' +
                 '<div class="mt-3 col-12" style="display:none" id="div4">' +
-                '<textarea id="descri" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
+                '<textarea id="descri4" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
                 '</div>';
             document.getElementById('cuestionario').innerHTML += '<div class="mt-3 col-12">' +
                 '<label for="pregunta5" id="pr_5" class="form-label"> ' + 5 + ' ' + rowData['cues5'] + '</label>' +
@@ -119,7 +119,7 @@ $("#tb_rd").on('click', 'tr', function () {
                 '</div>' +
                 '</div>' +
                 '<div class="mt-3 col-12" style="display:none" id="div5">' +
-                '<textarea id="descri" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
+                '<textarea id="descri5" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
                 '</div>';
             document.getElementById('cuestionario').innerHTML += '<div class="mt-3 col-12">' +
                 '<label for="pregunta6" id="pr_6" class="form-label"> ' + 6 + ' ' + rowData['cues6'] + '</label>' +
@@ -139,7 +139,7 @@ $("#tb_rd").on('click', 'tr', function () {
                 '</div>' +
                 '</div>' +
                 '<div class="mt-3 col-12" style="display:none" id="div6">' +
-                '<textarea id="descri" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
+                '<textarea id="descri6" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
                 '</div>';
             document.getElementById('cuestionario').innerHTML += '<div class="mt-3 col-12">' +
                 '<label for="pregunta7" id="pr_7" class="form-label"> ' + 7 + ' ' + rowData['cues7'] + '</label>' +
@@ -159,7 +159,7 @@ $("#tb_rd").on('click', 'tr', function () {
                 '</div>' +
                 '</div>' +
                 '<div class="mt-3 col-12" style="display:none" id="div7">' +
-                '<textarea id="descri" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
+                '<textarea id="descri7" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
                 '</div>';
             $('#mostrarDiv1').on('click', function () {
                 $('#div1').show();
@@ -222,6 +222,20 @@ $("#tb_rd").on('click', 'tr', function () {
                 let cue12 = '';
                 let cue13 = '';
                 let cue14 = '';
+                let come1 = $('#descri1').val();
+                let come2 = $('#descri2').val();
+                let come3 = $('#descri3').val();
+                let come4 = $('#descri4').val();
+                let come5 = $('#descri5').val();
+                let come6 = $('#descri6').val();
+                let come7 = $('#descri7').val();
+                let come8 = '';
+                let come9 = '';
+                let come10 = '';
+                let come11 = '';
+                let come12 = '';
+                let come13 = '';
+                let come14 = '';
                 responder_cuest(
                     fecha,
                     respid,
@@ -240,7 +254,21 @@ $("#tb_rd").on('click', 'tr', function () {
                     cue13,
                     cue14,
                     area,
-                    subarea)
+                    subarea,
+                    come1,
+                    come2,
+                    come3,
+                    come4,
+                    come5,
+                    come6,
+                    come7,
+                    come8,
+                    come9,
+                    come10,
+                    come11,
+                    come12,
+                    come13,
+                    come14)
             })
 
         }
@@ -284,7 +312,7 @@ $("#tb_rs").on('click', 'tr', function () {
                 '</div>' +
                 '</div>' +
                 '<div class="mt-3 col-12" style="display:none" id="div11">' +
-                '<textarea id="descri" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
+                '<textarea id="descri11" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
                 '</div>';
             document.getElementById('cuestionario').innerHTML += '<div class="mt-3 col-12">' +
                 '<label for="pregunta12" id="pr_12" class="form-label"> ' + 2 + ' ' + rowData['cues12'] + '</label>' +
@@ -304,7 +332,7 @@ $("#tb_rs").on('click', 'tr', function () {
                 '</div>' +
                 '</div>' +
                 '<div class="mt-3 col-12" style="display:none" id="div12">' +
-                '<textarea id="descri" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
+                '<textarea id="descri12" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
                 '</div>';
             document.getElementById('cuestionario').innerHTML += '<div class="mt-3 col-12">' +
                 '<label for="pregunta13" id="pr_13" class="form-label"> ' + 3 + ' ' + rowData['cues13'] + '</label>' +
@@ -324,7 +352,7 @@ $("#tb_rs").on('click', 'tr', function () {
                 '</div>' +
                 '</div>' +
                 '<div class="mt-3 col-12" style="display:none" id="div13">' +
-                '<textarea id="descri" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
+                '<textarea id="descri13" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
                 '</div>';
             $('#mostrarDiv11').on('click', function () {
                 $('#div11').show();
@@ -363,6 +391,20 @@ $("#tb_rs").on('click', 'tr', function () {
                 let cue12 = $('input[name="pr_12"]:checked').val();
                 let cue13 = $('input[name="pr_13"]:checked').val();
                 let cue14 = '';
+                let come1 = '';
+                let come2 = '';
+                let come3 = '';
+                let come4 = '';
+                let come5 = '';
+                let come6 = '';
+                let come7 = '';
+                let come8 = '';
+                let come9 = '';
+                let come10 = '';
+                let come11 = $('#descri11').val();
+                let come12 = $('#descri12').val();
+                let come13 = $('#descri13').val();
+                let come14 = '';
                 responder_cuest(
                     fecha,
                     respid,
@@ -381,7 +423,21 @@ $("#tb_rs").on('click', 'tr', function () {
                     cue13,
                     cue14,
                     area,
-                    subarea)
+                    subarea,
+                    come1,
+                    come2,
+                    come3,
+                    come4,
+                    come5,
+                    come6,
+                    come7,
+                    come8,
+                    come9,
+                    come10,
+                    come11,
+                    come12,
+                    come13,
+                    come14)
             })
         }
 
@@ -422,9 +478,9 @@ $("#tb_rm").on('click', 'tr', function () {
                 '<input class="form-check-input" value="NA" type="radio" name="pr_8" checked id="ocultarDiv8">' +
                 '<label class="form-check-label">NA</label>' +
                 '</div>' +
-                '</div>'+
+                '</div>' +
                 '<div class="mt-3 col-12" style="display:none" id="div8">' +
-                '<textarea id="descri" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
+                '<textarea id="descri8" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
                 '</div>';
             document.getElementById('cuestionario').innerHTML += '<div class="mt-3 col-12">' +
                 '<label for="pregunta9" id="pr_9" class="form-label"> ' + 2 + ' ' + rowData['cues9'] + '</label>' +
@@ -442,9 +498,9 @@ $("#tb_rm").on('click', 'tr', function () {
                 '<label class="form-check-label">NA</label>' +
                 '</div>' +
                 '</div>' +
-                '</div>'+
+                '</div>' +
                 '<div class="mt-3 col-12" style="display:none" id="div9">' +
-                '<textarea id="descri" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
+                '<textarea id="descri9" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
                 '</div>';
             document.getElementById('cuestionario').innerHTML += '<div class="mt-3 col-12">' +
                 '<label for="pregunta10" id="pr_10" class="form-label"> ' + 3 + ' ' + rowData['cues10'] + '</label>' +
@@ -462,9 +518,9 @@ $("#tb_rm").on('click', 'tr', function () {
                 '<label class="form-check-label">NA</label>' +
                 '</div>' +
                 '</div>' +
-                '</div>'+
+                '</div>' +
                 '<div class="mt-3 col-12" style="display:none" id="div10">' +
-                '<textarea id="descri" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
+                '<textarea id="descri10" class="form-control" rows="4" style="font-size: small;"placeholder="Escribe aquí tu comentario..."></textarea>' +
                 '</div>';
             document.getElementById('cuestionario').innerHTML += '<div class="mt-3 col-12">' +
                 '<label for="pregunta14" id="pr_14" class="form-label"> ' + 4 + ' ' + rowData['cues14'] + '</label>' +
@@ -482,9 +538,9 @@ $("#tb_rm").on('click', 'tr', function () {
                 '<label class="form-check-label">NA</label>' +
                 '</div>' +
                 '</div>' +
-                '</div>'+
+                '</div>' +
                 '<div class="mt-3 col-12" style="display:none" id="div14">' +
-                '<textarea id="descri" placeholder="Escribe aquí tu comentario..." class="form-control" rows="4" style="font-size: small;"></textarea>' +
+                '<textarea id="descri14" placeholder="Escribe aquí tu comentario..." class="form-control" rows="4" style="font-size: small;"></textarea>' +
                 '</div>';
             $('#mostrarDiv8').on('click', function () {
                 $('#div8').show();
@@ -510,7 +566,7 @@ $("#tb_rm").on('click', 'tr', function () {
             $('#ocultarDiv14').on('click', function () {
                 $('#div14').hide();
             });
-            
+
             $('.reali').off('click').on('click', function () {
                 let fecha = rowData['resp_fecha'];
                 let respid = rowData['resp_id'];
@@ -523,13 +579,27 @@ $("#tb_rm").on('click', 'tr', function () {
                 let cue5 = '';
                 let cue6 = '';
                 let cue7 = '';
-                let cue8 = $('input[name="pr_8"]:checked').val();;
-                let cue9 = $('input[name="pr_9"]:checked').val();;
-                let cue10 = $('input[name="pr_10"]:checked').val();;
+                let cue8 = $('input[name="pr_8"]:checked').val();
+                let cue9 = $('input[name="pr_9"]:checked').val();
+                let cue10 = $('input[name="pr_10"]:checked').val();
                 let cue11 = '';
                 let cue12 = '';
                 let cue13 = '';
-                let cue14 = $('input[name="pr_14"]:checked').val();;
+                let cue14 = $('input[name="pr_14"]:checked').val();
+                let come1 = '';
+                let come2 = '';
+                let come3 = '';
+                let come4 = '';
+                let come5 = '';
+                let come6 = '';
+                let come7 = '';
+                let come8 = $('#descri8').val();
+                let come9 = $('#descri9').val();
+                let come10 = $('#descri10').val();
+                let come11 = '';
+                let come12 = '';
+                let come13 = '';
+                let come14 = $('#descri14').val();
                 responder_cuest(
                     fecha,
                     respid,
@@ -548,7 +618,21 @@ $("#tb_rm").on('click', 'tr', function () {
                     cue13,
                     cue14,
                     area,
-                    subarea)
+                    subarea,
+                    come1,
+                    come2,
+                    come3,
+                    come4,
+                    come5,
+                    come6,
+                    come7,
+                    come8,
+                    come9,
+                    come10,
+                    come11,
+                    come12,
+                    come13,
+                    come14)
             })
         }
 
@@ -839,7 +923,21 @@ function responder_cuest(
     cue13,
     cue14,
     area,
-    subarea) {
+    subarea,
+    come1,
+    come2,
+    come3,
+    come4,
+    come5,
+    come6,
+    come7,
+    come8,
+    come9,
+    come10,
+    come11,
+    come12,
+    come13,
+    come14) {
     $.ajax({
         url: '../controller/cuestionario/respo_cuest.php',
         type: 'POST',
@@ -862,10 +960,22 @@ function responder_cuest(
             cue13: cue13,
             cue14: cue14,
             area: area,
-            subarea: subarea
+            subarea: subarea,
+            come1: come1,
+            come2: come2,
+            come3: come3,
+            come4:come4,
+            come5:come5,
+            come6:come6,
+            come7:come7,
+            come8:come8,
+            come9:come9,
+            come10:come10,
+            come11:come11,
+            come12:come12,
+            come13:come13,
+            come14:come14
         }
-
-
 
     }).done(function (resp) {
         console.log(resp);
